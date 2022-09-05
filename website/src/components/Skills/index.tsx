949aac4036a4cdd6091dbbb31a9a59900afc8d5e
afilazzola <alex.filazzola@outlook.com>
2022-09-04 22:16:11 -0400

    Got the skills set formatted

--- website/src/components/Skills/index.tsx
+++ website/src/components/Skills/index.tsx
@@ -1,28 +1,12 @@
-import { h } from 'preact';
 import Styles from './styles.module.scss';
 
 
 export default function SkillSets() {
 
-	const skills = {
-		"SkillType": [
-			"Data Science","Data Science","Data Science","Data Science","Data Science",
-			"Web Dev","Web Dev","Web Dev","Web Dev","Web Dev",
-			"Analytics","Analytics","Analytics","Analytics","Analytics"],
-		"Skill": [
-			"R","Git","Bash","Python","SQL",
-			"JS/TS","HTML/CSS","Node","Astro","React",
-			"GIS","GLMMs","Data Viz","Ordinations","ML"],
-		"SkillLevel": [
-			"100%","80%","80%","50%","40%",
-			"80%","80%","60%","50%","50%",
-			"100%","90%","90%","80%","50%"]
-	}
-
 	const allSkills = [
 		"R","Git","Bash","Python","SQL",
 		"JS/TS","HTML/CSS","Node","Astro","React",
-		"GIS","GLMMs","Data Viz","Ordinations","ML"]
+		"GIS","GLMMs","Data Viz","Ordinations","Machine Learning"]
 
 	const skillLevels = [
 		
@@ -34,15 +18,13 @@ export default function SkillSets() {
 
 
 	return(
-<div> 
-	  <div className={Styles.skillSets}>
-		<h2 className="text-xl font-bold py-4"> {skills.SkillType[0]} </h2>
-	<div className = {Styles.row}>
-			{allSkills.map((skill, index) => {
+	  <div className={Styles.grid}>
+		<div className="column">
+		<h2 className="text-xl font-bold py-4"> Data Science</h2>
+			{allSkills.slice(0, 5).map((skill, index) => {
 				return(
-					
-		<div className = {Styles.column}>
-		<h2 className="text-lg py-2"> {skill} </h2>
+					<div>
+					<h2 className="text-lg py-2"> {skill} </h2>
 			
 	<div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
 	  <div className="bg-orange-400 h-2.5 rounded-full" style={{width: skillLevels[index]}}></div>
@@ -50,8 +32,35 @@ export default function SkillSets() {
 	</div>
 			)})}
 	</div>
+	<div className="column">
+		<h2 className="text-xl font-bold py-4"> Web Dev</h2>
+			{allSkills.slice(5, 10).map((skill, index) => {
+				return(
+					<div>
+					
+
+		<h2 className="text-lg py-2"> {skill} </h2>
+			
+	<div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+	  <div className="bg-orange-400 h-2.5 rounded-full" style={{width: skillLevels[index+5]}}></div>
 	</div>
-	
+	</div>
+			)})}
 			</div>
+			<div className="column">
+		<h2 className="text-xl font-bold py-4"> Analytics</h2>
+			{allSkills.slice(10, 15).map((skill, index) => {
+				return(
+					<div>
+					<h2 className="text-lg py-2"> {skill} </h2>
+				<div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+	  <div className="bg-orange-400 h-2.5 rounded-full" style={{width: skillLevels[index+10]}}></div>
+	</div>
+	</div>
+			)})}
+			</div>
+				</div>
+
+	
 	)
 	}

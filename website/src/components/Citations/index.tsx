e59f82ef559f5500b86ac3806f753febf576db3d
afilazzola <alex.filazzola@outlook.com>
2022-09-05 17:17:19 -0400

    Finished building out the about page

---
+++ website/src/components/Citations/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './styles.module.scss';
+
+
+const Citations = () => {
+
+	return(
+
+	)
+}
+
+export default Citations
--- website/src/components/Nav/index.tsx
+++ website/src/components/Nav/index.tsx
@@ -7,11 +7,17 @@ function Nav() {
 			<a className={Styles.logolink} href="/">
 				<div className={Styles.monogram}>JW</div>
 			</a>
+			<a className={Styles.link} href="/">
+				Home
+			</a>
 			<a className={Styles.link} href="/projects">
 				Portfolio
 			</a>
 			<a className={Styles.link} href="/about">
 				About
+			</a>
+			<a className={Styles.link} href="/publications">
+				Publications
 			</a>
 			<a className={Styles.social} href="https://twitter.com/EcoFilazzola">
 				<svg className={Styles.socialicon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">

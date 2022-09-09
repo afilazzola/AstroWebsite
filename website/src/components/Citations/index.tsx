44f24567cc5c75295799ba5bb126f76953668c72
afilazzola <alex.filazzola@outlook.com>
2022-09-08 21:48:00 -0400

    Finished adding publications to publication page

--- website/src/components/Citations/index.tsx
+++ website/src/components/Citations/index.tsx
@@ -8,11 +8,11 @@ export const CitationsFormatted = ( ) => {
 
 	return(
 		<div>
-			{publications.map((citation) => {
+			{publications.map((citation, index) => {
 				     
 				return (
 					<div className="grid grid-flow-col gap-4">
-					<h6> {citation.Authors} ({citation.Year}) {citation.Title} {citation.Journal} <a href={citation.Link}> Link </a> </h6> 
+					<h6>{index+1}. {citation.Authors} ({citation.Year}) {citation.Title} {citation.Journal} <a href={citation.Link}> Link </a> </h6> 
 					</div>
 				)})}
 		</div>

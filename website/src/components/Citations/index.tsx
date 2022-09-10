fd200ec2b80f8d9c95287c0753e49164a35bb5e1
afilazzola <alex.filazzola@outlook.com>
2022-09-10 13:18:55 -0400

    Added photo to home page

--- website/src/components/Citations/index.tsx
+++ website/src/components/Citations/index.tsx
@@ -1,20 +1,19 @@
 import Styles from './styles.module.scss';
-import type {Citations} from './types';
+import type { Citations } from './types';
 import publications from "../../../public/publications.json"
 
-export const CitationsFormatted = ( ) => {
+export const CitationsFormatted = () => {
 
-
-
-	return(
+	return (
 		<div>
 			{publications.map((citation, index) => {
-				     
+
 				return (
 					<div className="grid grid-flow-col gap-4">
-					<h6>{index+1}. {citation.Authors} ({citation.Year}) {citation.Title} {citation.Journal} <a href={citation.Link}> Link </a> </h6> 
+						<h6>{index + 1}. {citation.Authors} ({citation.Year}) {citation.Title} {citation.Journal} <a href={citation.Link}> Link </a> </h6>
 					</div>
-				)})}
+				)
+			})}
 		</div>
 	)
 }
